362a424df42709ec4148a16faa4c3a618b898fa3
RobertBisiorowski <bisiorowskijr@gmail.com>
2017-07-30 11:42:08 +0200

    First one again

--- app/src/main/java/com/example/bisio/eat_on/MainActivity.java
+++ app/src/main/java/com/example/bisio/eat_on/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.bisio.eat_on;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
         allRecipesBtn = (Button)findViewById(R.id.button);
         searchByProductBtn = (Button)findViewById(R.id.button2);
         addRecipeBtn = (Button)findViewById(R.id.button3);
+
+        // Create a TypeFace using font from Assets folder
+        Typeface mtypeFace = Typeface.createFromAsset(getAssets(), "fonts/moonflower.TTF");
+        // set TypeFace to the TextView or Edittext
+        allRecipesBtn.setTypeface(mtypeFace);
+        searchByProductBtn.setTypeface(mtypeFace);
+        addRecipeBtn.setTypeface(mtypeFace);
 
         allRecipesBtn.setOnClickListener(new View.OnClickListener(){
             @Override
